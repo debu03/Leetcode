bc65483cc06b28158b94246ed9d42b41ad371d16
Debanjana Debjyoti Chanda <debu@Debanjanas-MacBook-Air.local>
2023-08-27 12:51:47 +0530

    day4

---
+++ two_sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums, target): #create a list to search from and traget sum
+        
+        hashMap= {} #create a dictionary called hashMap to map key and value
+        for index, num in enumerate(nums): #iterate over enumerate-->get index of num in list
+            diff = target - num #if diff is present in list means sum is posible
+            if diff in hashMap: 
+                return [hashMap[diff], index] #return index of hashmap
+            hashMap[num] = index #add index to hashmap
+
+
+            #simplest solution in python time and space complexity O(n)
